77c4e223d8ae3681be95ca52fcb12e5e3c286cef
KrzysztofNawara <5757040+KrzysztofNawara@users.noreply.github.com>
2020-09-26 20:59:17 +0200

    add to plot_convergence ability to override axes + add jupyter notebook for experiments (#1)

--- GPyOpt/core/bo.py
+++ GPyOpt/core/bo.py
@@ -287,7 +287,7 @@ class BO(object):
             model_to_plot = self.model
 
         return plot_acquisition(self.acquisition.space.get_bounds(),
-                                model_to_plot.model.X.shape[1],
+                                 model_to_plot.model.X.shape[1],
                                 model_to_plot.model,
                                 model_to_plot.model.X,
                                 model_to_plot.model.Y,
@@ -297,14 +297,14 @@ class BO(object):
                                 label_x,
                                 label_y)
 
-    def plot_convergence(self,filename=None):
+    def plot_convergence(self,filename=None, axes=None):
         """
         Makes twp plots to evaluate the convergence of the model:
             plot 1: Iterations vs. distance between consecutive selected x's
             plot 2: Iterations vs. the mean of the current model in the selected sample.
         :param filename: name of the file where the plot is saved
         """
-        return plot_convergence(self.X,self.Y_best,filename)
+        return plot_convergence(self.X,self.Y_best,filename,axes)
 
     def get_evaluations(self):
         return self.X.copy(), self.Y.copy()
